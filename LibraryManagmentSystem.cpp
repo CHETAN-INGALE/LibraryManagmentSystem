9a9f4ae795ed440e38fe1b8121957179cead73e2
CHETAN INGALE <chetan.ingale007@gmail.com>
2023-07-10 11:28:45 +0530

    Removed Vector and make coded user frendly

--- LibraryManagmentSystem.cpp
+++ LibraryManagmentSystem.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <vector>
+#include <string>
 
 using namespace std;
 
@@ -10,7 +10,9 @@ public:
     string author;
     bool available;
 
-    Book(int id, string t, string a) : bookId(id), title(t), author(a), available(true) {}
+    Book() : bookId(0), title(""), author(""), available(true) {}
+
+    Book(int id, const string& t, const string& a) : bookId(id), title(t), author(a), available(true) {}
 };
 
 class Member {
@@ -18,25 +20,42 @@ public:
     int memberId;
     string name;
 
-    Member(int id, string n) : memberId(id), name(n) {}
+    Member() : memberId(0), name("") {}
+
+    Member(int id, const string& n) : memberId(id), name(n) {}
 };
 
 class Library {
 private:
-    vector<Book> books;
-    vector<Member> members;
+    static const int MAX_BOOKS = 100;
+    static const int MAX_MEMBERS = 100;
+
+    Book books[MAX_BOOKS];
+    Member members[MAX_MEMBERS];
+    int bookCount;
+    int memberCount;
 
 public:
-    void addBook(int bookId, string title, string author) {
-        Book book(bookId, title, author);
-        books.push_back(book);
-        cout << "Book added successfully!" << endl;
+    Library() : bookCount(0), memberCount(0) {}
+
+    void addBook(int bookId, const string& title, const string& author) {
+        if (bookCount < MAX_BOOKS) {
+            Book book(bookId, title, author);
+            books[bookCount++] = book;
+            cout << "Book added successfully!" << endl;
+        } else {
+            cout << "Maximum book capacity reached!" << endl;
+        }
     }
 
-    void addMember(int memberId, string name) {
-        Member member(memberId, name);
-        members.push_back(member);
-        cout << "Member added successfully!" << endl;
+    void addMember(int memberId, const string& name) {
+        if (memberCount < MAX_MEMBERS) {
+            Member member(memberId, name);
+            members[memberCount++] = member;
+            cout << "Member added successfully!" << endl;
+        } else {
+            cout << "Maximum member capacity reached!" << endl;
+        }
     }
 
     void issueBook(int bookId, int memberId) {
@@ -57,7 +76,7 @@ public:
 
     void returnBook(int bookId) {
         Book* book = findBook(bookId);
-        
+
         if (book) {
             if (!book->available) {
                 book->available = true;
@@ -72,7 +91,8 @@ public:
 
     void generateReport() {
         cout << "---- Books ----" << endl;
-        for (const auto& book : books) {
+        for (int i = 0; i < bookCount; ++i) {
+            const Book& book = books[i];
             cout << "Book ID: " << book.bookId << endl;
             cout << "Title: " << book.title << endl;
             cout << "Author: " << book.author << endl;
@@ -81,7 +101,8 @@ public:
         }
 
         cout << "---- Members ----" << endl;
-        for (const auto& member : members) {
+        for (int i = 0; i < memberCount; ++i) {
+            const Member& member = members[i];
             cout << "Member ID: " << member.memberId << endl;
             cout << "Name: " << member.name << endl;
             cout << endl;
@@ -90,18 +111,18 @@ public:
 
 private:
     Book* findBook(int bookId) {
-        for (auto& book : books) {
-            if (book.bookId == bookId) {
-                return &book;
+        for (int i = 0; i < bookCount; ++i) {
+            if (books[i].bookId == bookId) {
+                return &books[i];
             }
         }
         return nullptr;
     }
 
     Member* findMember(int memberId) {
-        for (auto& member : members) {
-            if (member.memberId == memberId) {
-                return &member;
+        for (int i = 0; i < memberCount; ++i) {
+            if (members[i].memberId == memberId) {
+                return &members[i];
             }
         }
         return nullptr;
@@ -111,19 +132,74 @@ private:
 int main() {
     Library library;
 
-    library.addBook(1, "Applied Mathematics 1", "G.V. Kumbhojkar");
-    library.addBook(2, "Engineering physics 2", "I. A. Saikh");
-    library.addBook(3, "XII Computer Science Bifocal", "R. D. Supekar");
+    cout << "Welcome to the Library Management System!" << endl;
 
-    library.addMember(1, "Himanshu Bhangale");
-    library.addMember(2, "Chetan Ingale");
+    while (true) {
+        cout << "\nPlease select an option:" << endl;
+        cout << "1. Add a book" << endl;
+        cout << "2. Add a member" << endl;
+        cout << "3. Issue a book" << endl;
+        cout << "4. Return a book" << endl;
+        cout << "5. Generate report" << endl;
+        cout << "6. Exit" << endl;
 
-    library.issueBook(1, 1);
-    library.issueBook(3, 2);
+        int choice;
+        cin >> choice;
 
-    library.returnBook(3);
+        if (choice == 1) {
+            int bookId;
+            string title, author;
 
-    library.generateReport();
+            cout << "Enter book ID: ";
+            cin >> bookId;
+            cin.ignore();
+
+            cout << "Enter book title: ";
+            getline(cin, title);
+
+            cout << "Enter book author: ";
+            getline(cin, author);
+
+            library.addBook(bookId, title, author);
+        } else if (choice == 2) {
+            int memberId;
+            string name;
+
+            cout << "Enter member ID: ";
+            cin >> memberId;
+            cin.ignore();
+
+            cout << "Enter member name: ";
+            getline(cin, name);
+
+            library.addMember(memberId, name);
+        } else if (choice == 3) {
+            int bookId, memberId;
+
+            cout << "Enter book ID: ";
+            cin >> bookId;
+
+            cout << "Enter member ID: ";
+            cin >> memberId;
+
+            library.issueBook(bookId, memberId);
+        } else if (choice == 4) {
+            int bookId;
+
+            cout << "Enter book ID: ";
+            cin >> bookId;
+
+            library.returnBook(bookId);
+        } else if (choice == 5) {
+            library.generateReport();
+        } else if (choice == 6) {
+            break;
+        } else {
+            cout << "Invalid choice. Please try again." << endl;
+        }
+    }
+
+    cout << "Thank you for using the Library Management System. Goodbye!" << endl;
 
     return 0;
 }
